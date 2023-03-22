c7fdad8514263b723f312ba016511d8e80492c97
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-03-22 23:01:48 +0000

    c1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/**
+* main - my main
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	char s[10] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+	return (0);
+}
