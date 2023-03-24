f8676238ccf9aa5d869876bc4f4e8ffccacf1097
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-03-24 00:09:45 +0000

    c7

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,18 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 10; j++)
 			_putchar(j + 48);
+		_putchar('1');
+		_putchar('0');
+		_putchar('1');
+		_putchar('1');
+		_putchar('1');
+		_putchar('2');
+		_putchar('1');
+		_putchar('3');
+		_putchar('1');
+		_putchar('4');
 		_putchar('\n');
 	}
 }
