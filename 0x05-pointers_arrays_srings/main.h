9c8ebd9be155a0c1e46baddb5da2cb08ac6fdeb1
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-03-28 08:13:15 +0000

    c2

--- 0x05-pointers_arrays_srings/main.h
+++ 0x05-pointers_arrays_srings/main.h
@@ -1,13 +1,12 @@
-int _putchar(char);
-void reset_to_98(int);
-void swap_int(int, int);
-int _strlen(char*);
-void _puts(char);
-void print_rev(char);
+int _putchar(char *a);
+void reset_to_98(int *a);
+void swap_int(int *a, int *b);
+int _strlen(char *a);
+void _puts(char *a);
+void print_rev(char *a);
 void rev_string(char *g);
-void puts2(char);
-void puts_half(char);
-void print_array(int, int);
-char *_strcpy(char, char);
-int _atoi(char);
-
+void puts2(char *a);
+void puts_half(char *a);
+void print_array(int *a, int b);
+char *_strcpy(char *a, char *b);
+int _atoi(char *a);
