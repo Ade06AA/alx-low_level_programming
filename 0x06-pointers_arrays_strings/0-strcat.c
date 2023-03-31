9873dcc367f806dd8f7a7aaa077058f9f72ca4c4
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-03-31 03:02:06 +0000

    c5

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * _strcat - func name
 * @dest: func arg 1
@@ -9,8 +10,7 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; *dest != '\0'; i++)
-		dest = dest + i;
+	for (i = 0; *(dest + i)!= '\0'; i++);
 	for (j = 0; *src != '\0'; j++)
 		*(dest + i) = *(src + j);
 	return (dest);
