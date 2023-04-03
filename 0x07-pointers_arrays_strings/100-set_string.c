d8d2d9d69d6de0766d74ae8735334fa8eef58410
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-03 20:22:15 +0000

    c1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+/**
+* set_string - func name
+* @s: func arg 1 - pointer to pointer
+* @to: func arg 2 - pointer to variable
+* Reeturn: void
+*/
+void set_string(char **s, char *to)
+{
+	s = to;
+}
