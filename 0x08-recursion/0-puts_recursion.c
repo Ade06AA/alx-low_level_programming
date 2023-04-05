acfc22cf7416bfccea079147ae81271caa56c02f
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-05 17:52:45 +0000

    c1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _puts_recursion - func name
+* @s: func arg 1
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+	int i;
+
+	_putchar(*s);
+	i = 0;
+
+	if (*(s + i + 1) != '\0')
+	{
+	++i;
+	_puts_recursion(s + i);
+	if (*(s + i + 1) == '\0')
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/test.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+int main()
+{
+	char w[]="ade is a boy";
+
+	char *t = w;
+	_puts_recursion(t);
+	return 0;
+}
