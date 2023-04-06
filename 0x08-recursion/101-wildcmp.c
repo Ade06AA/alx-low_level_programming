c8b41ee02505b517da269b78cd45033a2f46fcd4
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-06 03:39:12 +0000

    c11

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+/**
+* wildcmp func name
+* @s1: func arg 1
+* @s2: func arg 2
+* Return: int
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	if (*s1 == '\0')
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		if (*s2 == '\0')
+			return (1);
+	}
+	if (*s1 == *s2)
+		return (wildcmp((s1 +1), (s2 + 1)));
+	return(0);
+	
+}
+
