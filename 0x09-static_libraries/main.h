3c59171331a6967aa17370e44bd111522d019b80
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-12 00:13:40 +0000

    c1

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,55 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void print_number(int);
+void reset_to_98(int *a);
+void swap_int(int *a, int *b);
+int _strlen(char *a);
+void _puts(char *a);
+void print_rev(char *a);
+void rev_string(char *g);
+void puts2(char *a);
+void puts_half(char *a);
+void print_array(int *a, int b);
+char *_strcpy(char *a, char *b);
+int _atoi(char *a);
+char *_strcat(char *dest, char *src);
+char *_strncat(char*dest, char *src, int n);
+char *_strncpy(char*dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void set_string(char **s, char *to);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int  factorial(int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
+int wildcmp(char *a,char *b);
+int is_palindrome(char *c);
