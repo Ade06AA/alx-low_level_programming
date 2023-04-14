67ac22863796667d77d023dc0e94183d41d79abc
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-14 15:59:57 +0000

    c4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *r;
 
-	r = malloc(new_size);
-
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == NULL)
@@ -22,6 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (0);
 	}
+	r = malloc(new_size);
 	if (r == NULL)
 		return (0);
 	if (new_size <= old_size)
