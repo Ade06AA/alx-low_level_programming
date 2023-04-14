268458cd9e06ddfd50c6196affeb479f41a8cd8f
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-14 15:13:37 +0000

    c1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,12 +20,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0)
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
 	if (r == NULL)
-		return (NULL);
-	if (new_size <= old_size)
-		return (ptr);
+		return (0);
 
 	if (old_size > 0)
 		memcpy(r, ptr, old_size);
