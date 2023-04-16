e266370983a61f699dc16d13ea2f55d9ddb0d6b2
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-16 22:27:31 +0000

    c3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,10 +13,11 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+	gp=malloc(sizeof(int*) * height);
 
 	for (i = 0; i < height; i++)
 	{
-		gp[i]=malloc(sizeof(int) * width);
+		gp[i]= malloc(sizeof(int) * 100);
 		if (gp[i] == NULL)
 			return (NULL);
 		for (j = 0; j < width; j++)
