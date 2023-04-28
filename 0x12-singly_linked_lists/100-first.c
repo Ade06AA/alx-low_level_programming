f05404fa2d04ee334d0df997bdc2207d72d0dae2
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-28 04:13:24 +0000

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
 */
 void __attribute__ ((constructor)) pbm(void)
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
