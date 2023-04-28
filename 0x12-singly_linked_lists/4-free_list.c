de92093326cf5f74634d47f6d04cd868214bb369
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-28 04:49:07 +0000

    c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_list - func name
+* @head: func arg 1
+* Return: void
+*/
+void free_list(list_t *h)
+{
+	list_t *c;
+
+	while ((c = h) != NULL)
+	{
+		h = h->next;
+		free(c->str);
+		free(c);
+	}
+}
