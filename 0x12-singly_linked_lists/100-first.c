6505a182ddd8527a9075d3adb7fccdc19d18cb32
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-28 03:43:42 +0000

    c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* pbm - func name
+* Return: void
+*/
+void __attribute__ ((constructor)) pbm(void)
+{
+	printf("You're beat! and yet, you must allow,\n")
+	printf("I bore my house upon my back!\n");
+}
