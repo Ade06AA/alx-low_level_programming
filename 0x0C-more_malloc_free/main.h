a6ef50924e72efb582d07517bae8bfb7cd78b51d
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-05-03 06:04:41 +0000

    c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,5 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int a);
 int *array_range(int, int);
+char *_memset(char, char, unsigned int);
+void *_calloc(unsigned int, unsigned int);
