5903144d4e13e514826e6d0563ae8287314c40e5
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-05-03 05:47:03 +0000

    c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* array_range - func name
+* @min: func arg 1
+* @max: func arg 2
+* Return: a
+*/
+int *array_range(int min, int max)
+{
+	int *a,
+	int i = 0,
+	int t = min;
+
+	if (min > max)
+		return (0);
+	a = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (0);
+	while ((max - min) >= i)
+		a[i++] = t++;
+	return (a);
+}
