392bf3cc22d565ce20fff5bbb8d6547124e0904d
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-05-03 06:34:06 +0000

    c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,9 @@
+#ifndef KK
+#define KK
 char *argstostr(unsigned int a, char **n);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int, char);
 void ch_free_grid(char, unsigned int);
 char **strtow(char *str);
-char *_strdup(char);
+char *_strdup(char *);
+#endif
