118b7dd61245d794c8e84b228ce8e7d9db141fdd
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-09-22 17:58:17 -0400

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int hash;
 	hash_node_t *node;
-	
+
 	if (!ht || !key)
 		return (NULL);
 	hash = key_index((const unsigned char *) key, ht->size);
