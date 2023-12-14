6f4a6f4c9b0dd802d9440a05941b165cd6d86914
OadeO6 <abdurrahman.adenowo06@gmail.com>
2023-12-14 23:48:05 +0100

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #ifndef __SA__
 #define __SA__
 
+#include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
